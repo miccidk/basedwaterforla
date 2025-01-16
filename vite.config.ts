cbe41a83b4b68a90843ce5e646a00f319d2f64f6
Michael <michael.a95@hotmail.dk>
2025-01-16 21:58:31 +0100

    switched to back

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-	build: {
-		outDir: 'docs',
-		// Optionally, if you want to clean the output directory before building:
-		emptyOutDir: true
-	}
 })
